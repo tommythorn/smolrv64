dcb969cd8cfde3dbc643d72f438dd9cee07a324c
Tommy Thorn <tommy-github2@thorn.ws>
2025-09-14 11:19:16 -0700

    run-riscv-tests.sh: use binary name smolrv64-tests to avoid name collision

--- run-riscv-tests.sh
+++ run-riscv-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # passes fails unsupported
-iverilog  -o smolrv64 -s smolrv64_tb -DSIMULATE -DRISCV_TESTS smolrv64.v rs232tx.v
+iverilog  -o smolrv64-tests -s smolrv64_tb -DSIMULATE -DRISCV_TESTS smolrv64.v rs232tx.v
 
 for class in $*
 do echo
@@ -8,6 +8,6 @@ do echo
    for x in riscv-tests/$class/*.bin
    do base=`basename $x .bin`
       printf "%-25s " $base
-      ./smolrv64 +even="riscv-tests/$class/$base.even" +odd="riscv-tests/$class/$base.odd"|egrep -v '(WARNING|finish called at)'
+      ./smolrv64-tests +even="riscv-tests/$class/$base.even" +odd="riscv-tests/$class/$base.odd"|egrep -v '(WARNING|finish called at)'
    done
 done
