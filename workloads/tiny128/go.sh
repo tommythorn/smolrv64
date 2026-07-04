93f1862d86d70e7e488179d92b6f472482ee71ce
Tommy Thorn <tommy-github2@thorn.ws>
2026-07-04 11:30:12 -0700

    tiny128 cosim + smaller 7.1 FW

---
+++ workloads/tiny128/go.sh
@@ -0,0 +1,1 @@
+cd ../../probe;NAME=tiny128 MEM_LG2=31 FW=../workloads/tiny128/fw_payload-7.1-small.bin DTB=../workloads/tiny128/tiny128.dtb INITRD=../workloads/tiny128/tiny128.cpio ./run-cosim-linux.sh
