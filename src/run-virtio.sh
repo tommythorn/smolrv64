7cf760e9bcb0d3a19d8cd0a612a669c09749783d
Tommy Thorn <tommy-github2@thorn.ws>
2026-08-03 12:58:01 -0700

    run-virtio: forward extra args to the binary (+ddr_real et al were silently dropped)

--- src/run-virtio.sh
+++ src/run-virtio.sh
@@ -66,4 +66,4 @@ INITRD_ARG=""; [ -n "$INITRD" ] && INITRD_ARG="+initrd=$INITRD"
 DISKRO_ARG=""; [ -n "${DISK:-}" ] && [ -z "${DISK_RW:-}" ] && DISKRO_ARG="+disk_ro"   # snapshot by default
 DISK_ARG=""; [ -n "${DISK:-}" ] && DISK_ARG="+disk=$DISK"
 IOFF_ARG=""; [ -n "${INITRD_OFF:-}" ] && IOFF_ARG="+initrd_off=$INITRD_OFF"
-exec "$BIN" +fw="$FW" +dtb="$DTB" $INITRD_ARG $IOFF_ARG $DISK_ARG $DISKRO_ARG $CKPT_ARG +a1="$A1" +cycles="$CYC"
+exec "$BIN" +fw="$FW" +dtb="$DTB" $INITRD_ARG $IOFF_ARG $DISK_ARG $DISKRO_ARG $CKPT_ARG +a1="$A1" +cycles="$CYC" "$@"
